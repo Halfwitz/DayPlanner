ca58b831b01ff3697ca9e9bb208102b0a8187263
Michael L <mlorenz1012@aol.com>
2025-01-25 15:55:21 -0500

    Update module-info exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,12 @@ module edu.snhu.dayplanner {
 
     opens edu.snhu.dayplanner to javafx.fxml;
     exports edu.snhu.dayplanner;
+    exports edu.snhu.dayplanner.service.appointmentservice;
+    opens edu.snhu.dayplanner.service.appointmentservice to javafx.fxml;
+    exports edu.snhu.dayplanner.service.contactservice;
+    opens edu.snhu.dayplanner.service.contactservice to javafx.fxml;
+    exports edu.snhu.dayplanner.service.taskservice;
+    opens edu.snhu.dayplanner.service.taskservice to javafx.fxml;
+    exports edu.snhu.dayplanner.control;
+    opens edu.snhu.dayplanner.control to javafx.fxml;
 }
