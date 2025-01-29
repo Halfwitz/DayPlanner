effb79984137919bd62510e3522ec7ddaf233a29
Michael L <mlorenz1012@aol.com>
2025-01-28 20:01:04 -0500

    Implement Task management screen
    Manage task objects in application with TaskController and TaskView

---
+++ src/main/java/edu/snhu/dayplanner/control/TaskController.java
@@ -0,0 +1,107 @@
+package edu.snhu.dayplanner.control;
+
+import edu.snhu.dayplanner.service.taskservice.Task;
+import edu.snhu.dayplanner.service.taskservice.TaskService;
+import edu.snhu.dayplanner.ui.TaskView;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+import java.util.List;
+
+/**
+ * Manages the logic for the Task section of the Day Planner Application. This handles user interactions,
+ * coordinating between {@code TaskService} and {@code TaskView}, and provides listener methods for handling
+ * adding, removing, and updating task entries depending on which GUI elements are used.
+ *
+ * @author Michael Lorenz
+ * @version 1.0, 1/25/2025
+ */
+public class TaskController
+{
+    private final TaskService tasks;
+    private final TaskView taskView;
+
+    private static final String CSV_FILE_PATH = "data/tasks.csv";
+
+    /**
+     * Initializes a controller instance that sets up the initial {@code TaskService} list and initializes a new
+     * {@code TaskView}. Configures event handlers for adding, removing, and editing tasks.
+     */
+    public TaskController() {
+        tasks = new TaskService();
+        tasks.addFromFile(CSV_FILE_PATH);
+
+        taskView = new TaskView(this::handleRemoveTask, this::handleEditTask);
+        taskView.getAddButton().setOnAction(event -> handleAddTask());
+        taskView.getSaveButton().setOnAction(event -> handleSaveTasks());
+    }
+
+    /**
+     * Handles adding a new task to the {@code taskView} table and {@code tasks} service.
+     * If inputs in taskView TextFields are correctly formatted, creates a new row in the table with action listeners
+     * for edit or remove actions and clears input fields. Else, it catches an {@code IllegalArgumentException} and logs
+     * to console, TODO: additional UI messages for this error should be indicated to user.
+     */
+    private void handleAddTask() {
+        try {
+            // retrieve input field references
+            List<String> input = taskView.getDataTable().getNewEntryInput();
+
+            // add task to tasks using input strings, throws IllegalArgumentException for invalid input
+            Task newTask = tasks.add(input.get(0), input.get(1));
+
+            // add new row to table and clear input forms.
+            taskView.getDataTable().clearNewEntryInput();
+            taskView.getDataTable().createDataRow(newTask);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Handles removing a task from the {@code tasks} service and task table view. This method is called by
+     * each Task row's inline delete button created in the view. The method is passed as paramater in
+     * {@code TaskView} constructors using {{@code BiConsumer} interface}
+     * @param task
+     * @param tableRow
+     */
+    private void handleRemoveTask(Task task, Node tableRow) {
+        tasks.delete(task); // FIXME: likely throws error if task does not exist. Unsure if this case is possible yet.
+        taskView.getDataTable().removeRow(tableRow);
+    }
+
+    /**
+     * Handles editing a task's specified field within the {@code tasks} service if task field is edited in
+     * table view.
+     * This is called by each Task row's text fields upon changes. An IllegalArgumentException is caught for invalid
+     * input and logs an error. FIXME: Should also indicate the input issue to the User
+     * @param task the task to modify
+     * @param field the Task.Field to be updated
+     * @param newValue the new value to update the task field to.
+     */
+    private void handleEditTask(Task task, Task.Field field, String newValue) {
+        try {
+            tasks.updateField(task.getId(), field, newValue.trim());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getClass() + e.getMessage());
+        }
+    }
+
+    /**
+     * Saves all tasks to the CSV file.
+     * Invoked when the user clicks the save button in the AppointmentView.
+     */
+    private void handleSaveTasks() {
+        tasks.writeToFile(CSV_FILE_PATH);
+    }
+
+    /**
+     * Refreshes the task table view and returns the root node containing the Task screen elements. Used in
+     * {@code NavController} to overlay the Task screen in the layout.
+     * @return the {@code Parent} node containing the {Task view elements}
+     */
+    public Parent getView() {
+        taskView.getDataTable().updateTable(tasks.getAll());
+        return taskView.getView();
+    }
+}
---
+++ src/main/java/edu/snhu/dayplanner/ui/TaskView.java
@@ -0,0 +1,88 @@
+package edu.snhu.dayplanner.ui;
+
+import edu.snhu.dayplanner.service.taskservice.Task;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+/**
+ * Represents the Task section user interface for managing tasks in the Day Planner Application. This class
+ * renders a view with a screen title, a labelled table of tasks with edit and remove controls, and input fields
+ * for adding new tasks.
+ * <p>
+ * This class defines the layout and each elements style using JavaFX components, and can dynamically update the
+ * task table using provided data. Logic is handled by the {@code TaskController} class. </p>
+ *
+ * @author Michael Lorenz
+ * @version 1.0, 1/25/2025
+ */
+public class TaskView {
+    private final VBox root;
+    private final TableView<Task, Task.Field> dataTable;
+    private final VBox tableView;
+    private Button addButton;
+    private Button saveButton;
+
+    private final BiConsumer<Task, Node> onRemove;
+    private final TriConsumer<Task, Task.Field, String> onEdit;
+
+    /**
+     * Initializes this object to set up the layout with each element as a child of the {@code root} Vbox. Responsible
+     * for creating the screen header, and empty task table view with elements for a row of column headers, table
+     * data, and a row for adding new tasks to the table data.
+     * @param onRemove the method implementation that handles logic for when a task is removed from the table.
+     * @param onEdit the method implementation that handles logic for when a task field is edited in the table.
+     *               Should use Task as reference to edited field, and Task.Field as the field that is being
+     *               modified, and String as the new value.
+     *
+     */
+    public TaskView(BiConsumer<Task, Node> onRemove, TriConsumer<Task, Task.Field, String> onEdit) {
+        // set event listeners
+        this.onRemove = onRemove;
+        this.onEdit = onEdit;
+
+        root = new VBox();
+        // create view heading
+        Label header = new Label("TASKS");
+
+        // initialize components and data table with columns for each field in Task.Field
+        dataTable =  new TableView<Task, Task.Field>(Arrays.asList(Task.Field.values()), onRemove, onEdit);
+        tableView = dataTable.getView();
+        addButton = dataTable.getAddButton();
+        saveButton = new Button("SAVE CHANGES");
+
+        root.getChildren().addAll(header, tableView, saveButton);
+
+    }
+
+    /**
+     * Return the parent VBox node containing each element of the Tasks screen.
+     * @return root node displaying Tasks title, task data table, and new task fields.
+     */
+    public Parent getView() {
+        return root;
+    }
+
+    public TableView<Task, Task.Field> getDataTable() {
+        return dataTable;
+    }
+
+    /**
+     * @return reference to new task add button element.
+     */
+    public Button getAddButton() {
+        return addButton;
+    }
+
+    /**
+     * @return reference to save task first name field element.
+     */
+    public Button getSaveButton() {
+        return saveButton;
+    }
+}
